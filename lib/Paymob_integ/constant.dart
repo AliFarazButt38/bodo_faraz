d64074d1e730af47e89f3e189beddee3d7ded718
Shargeel Ahmed <72449945+shargeel071@users.noreply.github.com>
2023-05-10 12:00:14 +0500

    10/5/2023 payment

--- lib/Paymob_integ/constant.dart
+++ lib/Paymob_integ/constant.dart
@@ -6,8 +6,8 @@ class ApiConstant{
   static var paymentKey = '/acceptance/payment_keys';
   static const getRefCode = '/acceptance/payments/pay';
 
-  static var apiKey = '';
+  static var apiKey = 'ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SnVZVzFsSWpvaWFXNXBkR2xoYkNJc0ltTnNZWE56SWpvaVRXVnlZMmhoYm5RaUxDSndjbTltYVd4bFgzQnJJam8yTWpJM01UWjkuVFlSR3o3eUtpQU01eG5ILU55Mmp0dHdkUUgyOG51LU1QTDB0LWJBTGxwT0E0eWpoRTRLdE9PWUtaOWlMTlBCazNKWUpNdzFuTnI3RG1iRVNvT3lMa2c=';
 
-  static const String integrationIdCard = 'ENTER_YOUR_INTEGRATION_ID';
-  static const String integrationIdKiosk = 'ENTER_YOUR_INTEGRATION_ID';
+  static const String integrationIdCard = '3078429';
+  static const String integrationIdKiosk = '3786179';
 }
